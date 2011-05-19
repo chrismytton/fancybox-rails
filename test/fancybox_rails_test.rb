4903ced85e278b15e40945aff6942dec6933da10
Chris Mytton <self@hecticjeff.net>
2011-05-19 19:48:38 +0100

    add fancybox js and css

--- test/fancybox_rails_test.rb
+++ test/fancybox_rails_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class FancyboxRailsTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Fancybox::Rails
+  def setup
+    @app = Dummy::Application
+  end
+
+  test "jquery.fancybox.js is found as an assert" do
+    assert_not_nil @app.assets['jquery.fancybox.js']
+  end
+
+  test "jquery.fancybox.css is found as an asset" do
+    assert_not_nil @app.assets['jquery.fancybox.css']
   end
 end

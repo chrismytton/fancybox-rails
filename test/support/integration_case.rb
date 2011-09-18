9947e6bd75ca4c539aa3feb608038db75a73aa5c
Chris Mytton <self@hecticjeff.net>
2011-09-18 18:12:54 +0100

    include Capybara::DSL

--- test/support/integration_case.rb
+++ test/support/integration_case.rb
@@ -1,6 +1,6 @@
 # Define a bare test case to use with Capybara
 class ActiveSupport::IntegrationCase < ActiveSupport::TestCase
-  include Capybara
+  include Capybara::DSL
   include Rails.application.routes.url_helpers
 
   def setup

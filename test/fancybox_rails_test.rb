63824ce5702d7a5ffa37943bba5562e7f82cdeab
Chris Mytton <self@hecticjeff.net>
2013-03-09 00:41:07 +0000

    Re-organize tests

--- test/fancybox_rails_test.rb
+++ test/fancybox_rails_test.rb
@@ -1,17 +1,34 @@
 require 'test_helper'
 
 class FancyboxRailsTest < ActiveSupport::TestCase
-  def setup
-    @app = Dummy::Application
+  include Rack::Test::Methods
+
+  def app
+    Dummy::Application
   end
 
   test "jquery.fancybox.js is found as an asset" do
-    assert_not_nil @app.assets['jquery.fancybox.js']
-    assert_not_nil @app.assets['fancybox.js']
+    assert_not_nil app.assets['jquery.fancybox.js']
+    assert_not_nil app.assets['fancybox.js']
   end
 
   test "jquery.fancybox.css is found as an asset" do
-    assert_not_nil @app.assets['jquery.fancybox.css']
-    assert_not_nil @app.assets['fancybox.css']
+    assert_not_nil app.assets['jquery.fancybox.css']
+    assert_not_nil app.assets['fancybox.css']
+  end
+
+  test "fancybox is included in application.js" do
+    get "/assets/application.js"
+    assert last_response.body.include?('FancyBox - jQuery Plugin')
+  end
+
+  test "fancybox css in included in application.css" do
+    get "/assets/application.css"
+    assert last_response.body.include?('FancyBox - jQuery Plugin')
+  end
+
+  test "fancybox assets are loaded" do
+    get "/assets/blank.gif"
+    assert_equal 200, last_response.status
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,19 @@ require "rails/test_help"
 Rails.backtrace_cleaner.remove_silencers!
 
 # Configure capybara for integration testing
-require "capybara/rails"
+require 'capybara/rails'
+require 'capybara/poltergeist'
 
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+# Define a bare test case to use with Capybara
+class ActiveSupport::IntegrationCase < ActiveSupport::TestCase
+  include Capybara::DSL
+  include Rails.application.routes.url_helpers
+
+  def setup
+    Capybara.current_driver = :poltergeist
+  end
+
+  def teardown
+    Capybara.use_default_driver
+  end
+end

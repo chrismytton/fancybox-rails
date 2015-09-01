5660808aa7a09baacf2ded2052f023f0e529b11b
Chris Mytton <chrismytton@gmail.com>
2015-09-01 10:19:16 +0100

    Fix tests to look for non-visible elements
    Capybara doesn't find hidden elements in its default configuration, you
    have to explicitly ask for them.

--- test/integration/fancybox_test.rb
+++ test/integration/fancybox_test.rb
@@ -4,7 +4,7 @@ class FancyboxTest < ActionDispatch::IntegrationTest
   test "fancybox is loaded and works" do
     visit root_path
 
-    fancybox = find('#fancybox-content')
+    fancybox = find('#fancybox-content', visible: false)
 
     assert !fancybox.visible?
 
